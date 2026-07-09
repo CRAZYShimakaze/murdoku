efc3c3b9443ddb5573b98c649deef7e6c8d1779b
Dirk <dirk.aporius@gmail.com>
2026-07-09 17:15:50 +0200

    small mobile adjustments

---
+++ docs/assets/esm-D8kJpWNd.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BRX6WJK0.js","./index-B-woH95R.js","./index-C7cU8ttz.css"])))=>i.map(i=>d[i]);
+import{i as e,t}from"./index-B-woH95R.js";var n;(function(e){e.Heavy=`HEAVY`,e.Medium=`MEDIUM`,e.Light=`LIGHT`})(n||={});var r;(function(e){e.Success=`SUCCESS`,e.Warning=`WARNING`,e.Error=`ERROR`})(r||={});var i=e(`Haptics`,{web:()=>t(()=>import(`./web-BRX6WJK0.js`).then(e=>new e.HapticsWeb),__vite__mapDeps([0,1,2]),import.meta.url)});export{i as Haptics,n as ImpactStyle,r as NotificationType};
